d5f972f58d0327940ba2663b62df2462475de0be
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-11-11 13:20:46 +0545

    Describe basic test suite for Ship

---
+++ src/test_suites/Ship.test.js
@@ -0,0 +1,25 @@
+describe("Ship basic tests", () => {
+  test("Ships have types", () => {
+    const shippy = new Ship("Battle");
+    expect(shippy.type).toBe("Battle");
+  });
+
+  test("Ships have default types", () => {
+    const shippy = new Ship();
+    expect(shippy.type).toBeDefined();
+    expect(shippy.length).toBeDefined();
+  });
+
+  test("Ships have lengths", () => {
+    const shippy = new Ship("Battle");
+    expect(shippy.length).toBeDefined();
+  });
+
+  test("Ship's types and length match", () => {
+    const patrolShip = new Ship("Patrol");
+    const carrierShip = new Ship("Carrier");
+
+    expect(patrolShip.length).toBe(2);
+    expect(carrierShip.length).toBe(5);
+  });
+});
