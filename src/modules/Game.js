551ffc3590f015ea6022a939c1cd4a35f33adcb6
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-11-11 15:49:21 +0545

    Calibrate the outofbound ships

--- src/modules/Game.js
+++ src/modules/Game.js
@@ -7,7 +7,36 @@ export default class Game {
     this.ships = [];
   }
 
+  calibrate(ship) {
+    let outOfBounds = false;
+    for (let i = 0; i < ship.occupied.length; ++i) {
+      if (ship.occupied[i].x > this.width - 1) {
+        outOfBounds = true;
+        break;
+      } else if (ship.occupied[i].x > this.height - 1) {
+        outOfBounds = true;
+        break;
+      }
+    }
+
+    if (outOfBounds) {
+      const newPosition = { x: 0, y: 0 };
+      if (ship.axis === "x") {
+        newPosition.x = ship.position.x - 1;
+        newPosition.y = ship.position.y;
+      } else {
+        newPosition.x = ship.position.x;
+        newPosition.y = ship.position.y - 1;
+      }
+
+      ship = this.calibrate(new Ship(ship.type, newPosition, ship.axis));
+    }
+
+    return ship;
+  }
+
   placeShip(type, position, axis) {
-    this.ships.push(new Ship(type, position, axis));
+    let placedShip = this.calibrate(new Ship(type, position, axis));
+    this.ships.push(placedShip);
   }
 }
