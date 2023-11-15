e470473e041fa6cf3d8ddca0135a167c2037398e
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-11-15 23:29:54 +0545

    Move gameboard's calibrate to placement

--- src/modules/GameBoard.js
+++ src/modules/GameBoard.js
@@ -30,46 +30,18 @@ export default class GameBoard {
     this.board[y][x] = token;
   }
 
-  calibrate(ship) {
-    let outOfBounds = false;
+  placeShip(ship) {
     for (let i = 0; i < ship.occupied.length; ++i) {
-      if (ship.occupied[i].x > this.width - 1) {
-        outOfBounds = true;
-        break;
-      } else if (ship.occupied[i].y > this.height - 1) {
-        outOfBounds = true;
-        break;
-      }
-    }
-
-    if (outOfBounds) {
-      const newPosition = { x: 0, y: 0 };
-      if (ship.axis === "x") {
-        newPosition.x = ship.position.x - 1;
-        newPosition.y = ship.position.y;
-      } else {
-        newPosition.x = ship.position.x;
-        newPosition.y = ship.position.y - 1;
-      }
-
-      ship = this.calibrate(new Ship(ship.type, newPosition, ship.axis));
-    }
-
-    return ship;
-  }
-
-  placeShip(type, position, axis) {
-    let placedShip = this.calibrate(new Ship(type, position, axis));
-
-    for (let i = 0; i < placedShip.occupied.length; ++i) {
-      const space = placedShip.occupied[i];
+      const space = ship.occupied[i];
       if (this.getSpace(space.x, space.y) !== " ") {
         return false;
       }
-
-      this.setSpace(space.x, space.y, placedShip.token);
     }
-    this.ships.push(placedShip);
+
+    ship.occupied.forEach((space) => {
+      this.setSpace(space.x, space.y, ship.token);
+    });
+    this.ships.push(ship);
     return true;
   }
 
