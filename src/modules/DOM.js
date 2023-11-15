fda93934f4c195df8073fb4b2f50bbbc771f1199
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-11-15 18:18:18 +0545

    Create IIFE module to load and get loaded document objs

---
+++ src/modules/DOM.js
@@ -0,0 +1,28 @@
+const DOM = (() => {
+  const elements = {};
+
+  function add(query) {
+    elements[query] = document.querySelector(query);
+  }
+
+  function load() {
+    add("player1-name");
+    add("player1-board");
+    add("player2-name");
+    add("player2-board");
+  }
+
+  // Returns the element if it has been loaded
+  function getElement(query) {
+    if (!elements[query]) {
+      console.error("Element not loaded or wrong query");
+      return;
+    }
+
+    return elements[query];
+  }
+
+  return { load, getElement };
+})();
+
+export default DOM;
