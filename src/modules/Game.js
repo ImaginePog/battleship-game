6d978a3311c044e3e17864768c8b3aa13409f1c0
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-11-15 20:22:49 +0545

    Render the board (both for now)

--- src/modules/Game.js
+++ src/modules/Game.js
@@ -22,6 +22,44 @@ export default class Game {
     )[0];
   }
 
+  render() {
+    this.players.forEach((player, index) => {
+      const playerName = DOM.getElement(`.player${index + 1}-name`);
+      playerName.innerText = player.name;
+
+      const board = DOM.getElement(`.player${index + 1}-board`);
+      board.innerText = "";
+
+      for (let y = 0; y < player.gameboard.width; ++y) {
+        const row = document.createElement("div");
+        row.classList.add("game-row");
+        for (let x = 0; x < player.gameboard.height; ++x) {
+          const cell = document.createElement("div");
+          cell.classList.add("game-cell");
+          cell.dataset.x = x;
+          cell.dataset.y = y;
+
+          const cellType = player.gameboard.getSpace(x, y);
+          switch (cellType) {
+            // empty space
+            case " ":
+              cell.classList.add("empty-cell");
+              break;
+            // Missed shot
+            case "X":
+              cell.classList.add("missed-cell");
+              break;
+            // Shot
+            case "S":
+              cell.classList.add("shot-cell");
+              break;
+          }
+          row.append(cell);
+        }
+        board.append(row);
+      }
+    });
+  }
 
   loop() {
     if (this.quit) {
