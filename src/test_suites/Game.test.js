f4b46cda2447e59d16adb5511ab755242ce0ca18
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-11-17 13:03:06 +0545

    Refactor Game test suite for new Game interface

--- src/test_suites/Game.test.js
+++ src/test_suites/Game.test.js
@@ -1,12 +1,9 @@
 import Game from "../modules/Game";
-import Player from "../modules/Player";
 
 describe("Game basic tests", () => {
   let game;
   beforeEach(() => {
-    const player1 = new Player("Player1");
-    const player2 = new Player("Noob");
-    game = new Game(player1, player2);
+    game = new Game("Player1");
   });
 
   test("Game has two players", () => {
@@ -16,6 +13,6 @@ describe("Game basic tests", () => {
   test("Game keeps track of turns", () => {
     expect(game.getCurrentPlayer().name).toBe("Player1");
     game.nextTurn();
-    expect(game.getCurrentPlayer().name).toBe("Noob");
+    expect(game.getCurrentPlayer().name).toBe("Computer");
   });
 });
