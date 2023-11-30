1e77020ac85717bd910d714ad8233a8d82d06c4a
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-12-01 03:19:06 +0545

    Improve Computer AI
    Computer now figures out what axis to focus on after
    finding the second hit in the adjacent square

--- src/modules/Computer.js
+++ src/modules/Computer.js
@@ -5,7 +5,7 @@ export default class Computer extends Player {
     super("Computer", drawBoard);
     this.targets = [];
     this.shotRecord = [];
-    this.fromPotentials = false;
+    this.lastHit = null;
   }
 
   getRandomCoord() {
@@ -46,6 +46,7 @@ export default class Computer extends Player {
     let target;
     if (this.targets.length === 0) {
       // No targets therefore shoot a random shootable coord
+      this.lastHit = null;
       target = this.getShootCoord();
     } else {
       // shoot a target square
@@ -53,13 +54,29 @@ export default class Computer extends Player {
     }
     let result = this.enemyBoard.takeShot(target.x, target.y);
     if (result == true) {
-      let newTargets = [
-        { x: target.x + 1, y: target.y },
-        { x: target.x, y: target.y + 1 },
-        { x: target.x - 1, y: target.y },
-        { x: target.x, y: target.y - 1 },
-      ];
-      this.fromPotentials = true;
+      let newTargets = [];
+      let knownAxis = null;
+      if (this.lastHit) {
+        if (target.x - this.lastHit.x == 0) {
+          knownAxis = "y";
+        }
+        if (target.y - this.lastHit.y == 0) {
+          knownAxis = "x";
+        }
+      }
+      if (knownAxis == "x") {
+        newTargets.push({ x: target.x + 1, y: target.y });
+        newTargets.push({ x: target.x - 1, y: target.y });
+      } else if (knownAxis == "y") {
+        newTargets.push({ x: target.x, y: target.y + 1 });
+        newTargets.push({ x: target.x, y: target.y - 1 });
+      } else {
+        newTargets.push({ x: target.x + 1, y: target.y });
+        newTargets.push({ x: target.x - 1, y: target.y });
+        newTargets.push({ x: target.x, y: target.y + 1 });
+        newTargets.push({ x: target.x, y: target.y - 1 });
+      }
+
       newTargets.forEach((newTarget) => {
         if (
           newTarget.x >= 0 &&
@@ -71,6 +88,8 @@ export default class Computer extends Player {
           this.targets.push(newTarget);
         }
       });
+
+      this.lastHit = target;
     }
 
     this.shotRecord.push(target);
