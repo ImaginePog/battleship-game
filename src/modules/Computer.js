c672a615a79d21809f1ef76722496ca13feb69bf
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-12-01 02:46:46 +0545

    Make Computer more intelligent
    Computer now records possible adjacent squares of a shot that has hit
    and continues shooting them until no more recorded square remains

--- src/modules/Computer.js
+++ src/modules/Computer.js
@@ -3,13 +3,39 @@ import Player from "./Player";
 export default class Computer extends Player {
   constructor(drawBoard) {
     super("Computer", drawBoard);
+    this.targets = [];
+    this.shotRecord = [];
+    this.fromPotentials = false;
   }
 
-  getRandomCoords() {
-    const shootX = Math.floor(Math.random() * this.enemyBoard.width);
-    const shootY = Math.floor(Math.random() * this.enemyBoard.height);
+  getRandomCoord() {
+    return {
+      x: Math.floor(Math.random() * this.enemyBoard.width),
+      y: Math.floor(Math.random() * this.enemyBoard.height),
+    };
+  }
 
-    return { x: shootX, y: shootY };
+  hasShot(coord) {
+    const found = this.shotRecord.findIndex(
+      (recorded) => recorded.x == coord.x && recorded.y == coord.y
+    );
+
+    if (found == -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  getShootCoord() {
+    let alreadyShot;
+    let shootCoord;
+    do {
+      shootCoord = this.getRandomCoord();
+      alreadyShot = this.hasShot(shootCoord);
+    } while (alreadyShot);
+
+    return shootCoord;
   }
 
   shoot() {
@@ -17,12 +43,37 @@ export default class Computer extends Player {
       return;
     }
 
-    let coords = this.getRandomCoords();
-    let result = this.enemyBoard.takeShot(coords.x, coords.y);
-    while (result === -1) {
-      coords = this.getRandomCoords();
-      result = this.enemyBoard.takeShot(coords.x, coords.y);
+    let target;
+    if (this.targets.length === 0) {
+      // No targets therefore shoot a random shootable coord
+      target = this.getShootCoord();
+    } else {
+      // shoot a target square
+      target = this.targets.pop();
     }
+    let result = this.enemyBoard.takeShot(target.x, target.y);
+    if (result == true) {
+      let newTargets = [
+        { x: target.x + 1, y: target.y },
+        { x: target.x, y: target.y + 1 },
+        { x: target.x - 1, y: target.y },
+        { x: target.x, y: target.y - 1 },
+      ];
+      this.fromPotentials = true;
+      newTargets.forEach((newTarget) => {
+        if (
+          newTarget.x >= 0 &&
+          newTarget.x < this.enemyBoard.width &&
+          newTarget.y >= 0 &&
+          newTarget.y < this.enemyBoard.height &&
+          !this.hasShot(newTarget)
+        ) {
+          this.targets.push(newTarget);
+        }
+      });
+    }
+
+    this.shotRecord.push(target);
     return result;
   }
 }
